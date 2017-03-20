6936dea89750a0c3ee43f7b1aec84826d4f8b2b1
jpalwal <jagoda.palwal@gmail.com>
2017-03-20 21:37:02 +0100

    Update mainProblem2.py

--- Word_counter/mainProblem2.py
+++ Word_counter/mainProblem2.py
@@ -1,1 +1,19 @@
+import argparse
+import validator
+from wordCounter import WordCounter
+from wordCounter import Logging
+
+parser=argparse.ArgumentParser()
+parser.add_argument("file_to_parse")
+parser.add_argument("file_for_logs")
+args=parser.parse_args()
+file_to_parse=args.file_to_parse
+file_for_logs=args.file_for_logs
+myValidator=validator.InputFileValidator(file_to_parse)
+
+if myValidator.validate():
+    myLogger=Logging(file_for_logs)
+    myLogger.logAnInfoMessage(WordCounter.letter_counter(file_to_parse))
+    myLogger.logAnInfoMessage(WordCounter.number_counter(file_to_parse))
+    myLogger.logAnInfoMessage(WordCounter.word_counter(file_to_parse))
 
