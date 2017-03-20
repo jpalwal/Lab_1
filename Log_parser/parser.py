d033b7f99dd4cb8188e4f54711bd89ae137850e7
jpalwal <jagoda.palwal@gmail.com>
2017-03-20 17:59:48 +0100

    Update parser.py

--- Log_parser/parser.py
+++ Log_parser/parser.py
@@ -1,23 +1,14 @@
-#!usr/bin/python3
+import os
 
-from sys import argv
-import argparse
+class NoFileFound(Exception):
+    pass
 
-str_to_find = "PrChecker.Downs"
-#wczytywanie nazwy pliku do przeszukiwania
-if len(argv)>1:
-    file_name = argv[1]
-else:
-    print("Nie podano pliku do odczytu, podaj jeszcze raz")
-    file_name = input()
-
-#otwieranie pliku i wczytywanie tekstu do listy
-if(type(file_name) == type("string")):
-    file=open('file_name','r')
-    try:
-        for line in file:
-            if str_to_find in line:
-                print(line)
-    finally:
-        file.close()
-
+class LogParser:
+    def parse_log(self,input_file,string_to_find):
+        if os.path.isfile(input_file):
+            file_to_check = open(input_file).read()
+            file_lines = file_to_check.split('\n')
+            lines_with_string = [line for line in file_lines if string_to_find in line]
+            return lines_with_string
+        else:
+            raise NoFileFound()
