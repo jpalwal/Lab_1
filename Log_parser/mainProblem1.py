54921878d360d964546493fdf71e1dadf38ad222
jpalwal <jagoda.palwal@gmail.com>
2017-09-08 18:44:30 +0200

    rename file

--- Log_parser/mainProblem1.py
+++ Log_parser/mainProblem1.py
@@ -17,7 +17,7 @@ myValidator=validator.InputFileValidator(file_to_parse)
 if myValidator.validate():
     parsed_data=""
     parsed_data+='\n'.join(pars.LogParser.parse_log(file_to_parse,string_to_find))
-    file_for_result=open("parsed1_data.csv",'w')
+    file_for_result=open("parsed_data.csv",'w')
     file_for_result.write(parsed_data)
     #df=pd.read_csv("parsed_data.csv")
     df=open("parsed_data.csv").read()
